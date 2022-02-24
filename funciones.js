c4d5df087a8a4a05d865ec18f07601ad37c66714
Ivan Otarola <iotarolal@gmail.com>
2022-02-24 00:56:33 -0300

    arreglo funcion agrega roommeantes, depuracion y eliminación de comentarios

--- funciones.js
+++ funciones.js
@@ -1,12 +1,5 @@
-const express = require('express');
 const axios = require('axios');
-const fs = require('fs').promises;
 const uuid = require('uuid');
-
-const app = express();
-
-app.use(express.static('static'));
-app.use(express.urlencoded({extended:true}));
 
 async function newroommate() {
 
@@ -14,21 +7,12 @@ async function newroommate() {
     randomuser = randomuser.data.results[0];
 
     const newuser = {
-        //crea un identificador
-        id : uuid.v4().slice(10),
-        nombre : randomuser.name.first + + randomuser.name.last,
-        debe:0,
-        recibe:0
+        id: uuid.v4().slice(10),
+        nombre: randomuser.name.first + " " + randomuser.name.last,
+        debe: 0,
+        recibe: 0
     }
-return newuser;
-}
-
-
-async function validonewroommate(){
-// valido que no se repina el mismo nombre
-
-
+    return newuser;
 }
 
 module.exports = newroommate;
-
--- server.js
+++ server.js
@@ -17,15 +17,14 @@ app.post('/roommate', async(req, res) => {
     const roommate = await newroommate();
     let db = await fs.readFile("db.json", 'utf-8');
     db = JSON.parse(db);
-
     db.roommates.push(roommate);
 
     await fs.writeFile('db.json', JSON.stringify(db), 'utf-8');
 
-    res.send("registro grabado")
+    res.send({ todo: "OK" });
 })
 
-
+// agrega gastos con post
 app.post('/gasto', async (req, res) => {
     let body;
     req.on('data', (payload) => {
@@ -48,53 +47,37 @@ app.post('/gasto', async (req, res) => {
         db.gastos.push(datosgasto);
 
         // Registro información en archivo db
-
         await fs.writeFile('db.json', JSON.stringify(db), 'UTF-8', function() {
             console.log('Gasto agregado');
         });
 
-        res.send(db);
+        res.send({ gastos: db.gastos });
 
     });
 });
 
 // obtengo datos de roommeantes 
-
 app.get('/roommates', async(req, res) => {
 
-    let bd = await fs.readFile("db.json", 'utf-8');
-    bd = JSON.parse(bd);
+    let db = await fs.readFile("db.json", 'utf-8');
+    db = JSON.parse(db);
 
-    console.log(bd.roommates)
-
-    for (romme of bd.roommates) {
+    for (romme of db.roommates) {
         // filtrar los gastos DE ESE romme, y sumar sus valores
-        for (gasto of bd.gastos) {
+        for (gasto of db.gastos) {
             if (gasto.roommate == romme.nombre) {
 
                 if (gasto.monto > 0) {
-                    console.log(gasto.monto);
                     romme.recibe = romme.recibe + gasto.monto;
-
                 } else {
                     romme.debe += gasto.monto;
-
                 }
             }
-
         }
     }
 
- //   console.log(romme.gastos);
+    res.send({ roommates: db.roommates });
 
-    res.send({ roommates: bd.roommates });
-
-    // Leer informacion del archivo db
-//    let db = await fs.readFile("db.json", 'utf-8');
-//    db = JSON.parse(db);
-
-    // retorno  datos al index.html
- //   res.send({roommates:db.roommates});
 });
 
 // obtengo datos de gastos
@@ -105,46 +88,41 @@ app.get('/gastos', async(req, res) => {
     db = JSON.parse(db);
 
     // retorno  datos al index.html
-    res.send({gastos:db.gastos});
-//    res.send(db);
+    res.send({ gastos: db.gastos });
 });
 
 // Put Update Gasto
-app.put('/gasto', async (req, res) => {
+app.put('/gasto', async(req, res) => {
 
-    const id = req.query.id;
     let body;
     req.on('data', (payload) => {
         body = JSON.parse(payload);
     });
-    
 
     req.on('end', async() => {
-    
-        const datosgasto = { 
-            id : req.query.id,
-            roommate: body.roommate, 
-            descripcion: body.descripcion, 
+
+        const datosgasto = {
+            id: req.query.id,
+            roommate: body.roommate,
+            descripcion: body.descripcion,
             monto: body.monto
         }
-        // Leer informacion del archivo db
+
         let db = await fs.readFile("db.json", 'utf-8');
         db = JSON.parse(db);
 
-        // Actualizo  datos de datosgasto a Gastos     
+        // busco registro a modificar
+        const gasto = db.gastos.find(g => g.id == datosgasto.id);
 
-        // elimino el registro modificado
-        const arrayroommate =  db.roommates.filter(x => x.id !== id);
-        const arraygastos =  db.gastos.filter(x => x.id !== id);
-        db = arrayroommate.concat(arraygastos);
+        // actualizo datos
+        gasto.roommate = datosgasto.roommate;
+        gasto.descripcion = datosgasto.descripcion;
+        gasto.monto = datosgasto.monto;
 
-        // agrego datos modificados
-        db.gastos.push(datosgasto);
-
-        // grabo registros
         await fs.writeFile('db.json', JSON.stringify(db), 'utf-8');
 
-        res.send(db);
+        // retorno  datos al index.html
+        res.send({ gastos: db.gastos });
 
     });
 });
@@ -154,11 +132,9 @@ app.put('/gasto', async (req, res) => {
 
 app.delete('/gasto', async (req, res) => {
 
-//    console.log('llego a eliminar gasto')
-
+    // obtengo id 
     const id = req.query.id;
-    console.log(id);
-
+ 
     // Leer informacion del archivo db
     let db = await fs.readFile("db.json", 'utf-8');
     db = JSON.parse(db);
@@ -168,8 +144,7 @@ app.delete('/gasto', async (req, res) => {
 
     await fs.writeFile('db.json', JSON.stringify(db), 'utf-8');
 
-    res.send(db);
-
+    res.send({ gastos: db.gastos });
 });
 
 
